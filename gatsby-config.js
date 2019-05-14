fa5de3d345d4abcc44ac88d951ac048ba2bfdd20
Ylli <yllipetrovci@gmail.com>
2019-05-14 21:30:53 +0200

    add google fonts, and plugins in gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Website`,
+    author: `ylli petrovci`,
+    siteUrl: 'localhost:8000'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +26,16 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `Raleway:100,400,300,500,600,700`, // you can also specify font weights and styles
+          `Nunito Sans`
+        ]
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
