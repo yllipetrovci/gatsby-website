c5ef7a2afc08cc56ad3544bfc89332a873bde1af
Ylli <yllipetrovci@gmail.com>
2019-05-18 01:15:05 +0200

    add expertise-card

---
+++ src/components/expertise-card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ExpertiseCard = (props) => {
+    return (
+        <div className="expertise-card-component">
+            <div className="row">
+                <div className="col-12">
+                    <img src={props.img} alt={props.header} />
+                </div>
+                <div className="col-12">
+                    <p>{props.paragraph}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpertiseCard;
