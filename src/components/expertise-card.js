e09652141627057ea269579e075f7f6755c45921
Ylli <yllipetrovci@gmail.com>
2019-05-18 13:46:20 +0200

    enhance expertice  card design

--- src/components/expertise-card.js
+++ src/components/expertise-card.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ExpertiseCard = (props) => {
     return (
-        <div className="expertise-card-component">
+        <div className="expertise-card-component pl-4 pr-4 pt-4 pb-4">
             <div className="row ">
                 <div className="col-12 ">
-                    <img src={props.img} alt={props.header} />
+                    <img src={props.img} alt={props.title} />
                 </div>
-                <div className="col-12 ">
-                    <h2>{props.header}</h2>
+                <div className="col-12 pt-3 pb-2">
+                    <h6>{props.title}</h6>
                 </div>
                 <div className="col-12 ">
                     <p>{props.paragraph}</p>
