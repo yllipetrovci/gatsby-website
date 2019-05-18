c09761e96a4152b05851c9beb1d906a392ce07d1
Ylli <yllipetrovci@gmail.com>
2019-05-18 12:19:06 +0200

    ehnace expertise card

--- src/components/expertise-card.js
+++ src/components/expertise-card.js
@@ -3,11 +3,14 @@ import React from 'react';
 const ExpertiseCard = (props) => {
     return (
         <div className="expertise-card-component">
-            <div className="row">
-                <div className="col-12">
+            <div className="row ">
+                <div className="col-12 ">
                     <img src={props.img} alt={props.header} />
                 </div>
-                <div className="col-12">
+                <div className="col-12 ">
+                    <h2>{props.header}</h2>
+                </div>
+                <div className="col-12 ">
                     <p>{props.paragraph}</p>
                 </div>
             </div>
