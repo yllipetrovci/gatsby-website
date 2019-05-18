1735487b9c47cb1e15f1dcea01d14b7a4300cb43
Ylli <yllipetrovci@gmail.com>
2019-05-18 14:00:23 +0200

    enhance navigation design

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,15 +9,14 @@ class Navigation extends Component {
                     <div className="col-3">
                         <img src={logo} alt="logo" />
                     </div>
-                    <div className="offset-1 col-8 pt-2 pr-0">
+                    <div className="offset-3 col-6 pt-2 pr-0">
                         <ul className="list-inline">
-                            <li className="list-inline-item">HOME</li>
-                            <li className="list-inline-item">ABOUT</li>
-                            <li className="list-inline-item">EXPERTISE</li>
-                            <li className="list-inline-item">TEAMS</li>
-                            <li className="list-inline-item">WORKS</li>
-                            <li className="list-inline-item">PEOPLE SAY</li>
-                            <li className="list-inline-item">PEOPLE SAY</li>
+                            <li className="list-inline-item mr-3">HOME</li>
+                            <li className="list-inline-item mr-3">ABOUT</li>
+                            <li className="list-inline-item mr-3">EXPERTISE</li>
+                            <li className="list-inline-item mr-3">TEAMS</li>
+                            <li className="list-inline-item mr-3">WORKS</li>
+                            <li className="list-inline-item mr-3">PEOPLE SAY</li>
                             <li className="list-inline-item">CONTACT</li>
                         </ul>
                     </div>
