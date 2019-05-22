bf0807944afaef74b4fbe95f3638f802ee55481b
Ylli <yllipetrovci@gmail.com>
2019-05-22 23:18:01 +0200

    add navigation button for small screen

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from '../images/logo.png';
 import '../styles/components/navigation.scss';
 class Navigation extends Component {
+
+    state = { openMobileNav: false };
+
+    handleClick = () => {
+        console.log('clicked');
+        this.setState({ openMobileNav: !this.state.openMobileNav });
+    }
+
     render() {
         return (
             <nav className="nav-component">
@@ -10,13 +18,28 @@ class Navigation extends Component {
                         <img src={logo} alt="logo" />
                     </div>
                     <div className="col-9 d-block d-md-none text-right">
-                        <div className="menu-icon">
-                            <div className="line"></div>
-                            <div className="line"></div>
-                            <div className="line"></div>
-                        </div>
+                        <button type="button" onClick={this.handleClick}>
+                            <div className="menu-icon">
+                                <div className="line"></div>
+                                <div className="line"></div>
+                                <div className="line"></div>
+                            </div>
+                        </button>
                     </div>
-                    <div className=" d-none d-md-block offset-3 col-6 pt-2 pr-0">
+
+                    {(this.state.openMobileNav) &&
+                        <div className="offset-8 col-4 mobile-nav-list">
+                            <ul className= {this.state.openMobileNav ? "active nav-mob-ul shadow": "nav-mob-ul shadow" } >
+                                <li className="text-center">HOME</li>
+                                <li className="text-center">ABOUT</li>
+                                <li className="text-center">EXPERTISE</li>
+                                <li className="text-center">TEAMS</li>
+                                <li className="text-center">WORKS</li>
+                                <li className="text-center">PEOPLE SAY</li>
+                                <li className="text-center">CONTACT</li>
+                            </ul>
+                        </div>}
+                    <div className="d-none d-md-block offset-lg-2 offset-md-0 col-md-9 col-lg-7 pt-2 pr-0 text-right">
                         <ul className="list-inline">
                             <li className="list-inline-item mr-3">HOME</li>
                             <li className="list-inline-item mr-3">ABOUT</li>
