5a54344c2e1512b6fb2436750644f3ae8a66a557
yllipetrovci <yllipetrovci@gmail.com>
2019-05-27 00:35:39 +0200

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const Contact = () => (
+  <Layout>
+    <h1>Contact </h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Contact
