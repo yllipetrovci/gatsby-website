2b10274335e44187b57680843d9490c4484785f6
yllipetrovci <yllipetrovci@gmail.com>
2019-05-27 00:32:37 +0200

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <h1>About </h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default About
