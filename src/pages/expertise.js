d04bd2077987037b17b20c1410f0519a1a0491fc
yllipetrovci <yllipetrovci@gmail.com>
2019-05-27 00:33:15 +0200

    add expertise page

---
+++ src/pages/expertise.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const Expertise = () => (
+  <Layout>
+    <h1>Expetise </h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Expertise
