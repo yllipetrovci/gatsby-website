42e8cac0690e4cffd7166dd052f74868256376d0
yllipetrovci <yllipetrovci@gmail.com>
2019-05-27 00:34:05 +0200

    add work page

---
+++ src/pages/works.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const Works = () => (
+  <Layout>
+    <h1>Works </h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Works
